1537358edb9ca8b409f195f517ff8a283ea9134a
Oliver Baker <avilebroker@gmail.com>
2020-02-04 15:47:54 -0600

    #5 Add basic storybook support, still needs old boilerplate code removed

---
+++ packages/hs-angular-ui/src/stories/Welcome.stories.ts
@@ -0,0 +1,17 @@
+import { Button } from '@storybook/angular/demo';
+
+export default { title: 'My Button' }
+
+export const withText = () => ({
+  component: Button,
+  props: {
+    text: 'Hello Button',
+  },
+});
+
+export const withEmoji = () => ({
+  component: Button,
+  props: {
+    text: '😀 😎 👍 💯',
+  },
+});
