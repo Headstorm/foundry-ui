be0d7c0251a4dbca6077fd44f7fd0aca69d392e8
Oliver Baker <avilebroker@gmail.com>
2020-02-07 12:57:05 -0600

    #5 WIP adding docs and actions add-ons

--- packages/hs-angular-ui/.storybook/main.js
+++ packages/hs-angular-ui/.storybook/main.js
@@ -1,6 +1,8 @@
 module.exports = {
   addons: [
     '@storybook/addon-a11y/register',
+    '@storybook/addon-actions/register',
+    '@storybook/addon-docs/register',
     '@storybook/addon-knobs/register',
   ],
   stories: ['../src/**/*.stories.[tj]s']
