bcaa4b3ed91a5ba5aa4492f4cf88e7ed49bc82b7
Oliver Baker <avilebroker@gmail.com>
2020-02-11 10:15:12 -0600

    #5 Fully pull out Docs add-on

--- packages/hs-angular-ui/.storybook/main.js
+++ packages/hs-angular-ui/.storybook/main.js
@@ -1,8 +1,7 @@
 module.exports = {
   addons: [
     '@storybook/addon-a11y/register',
-    '@storybook/addon-docs/register',
     '@storybook/addon-knobs/register',
   ],
-  stories: ['../src/**/*.stories.(ts|js|mdx)']
+  stories: ['../src/**/*.stories.[tj]s']
 };
