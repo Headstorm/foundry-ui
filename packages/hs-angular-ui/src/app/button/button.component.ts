2cb3c4aaa896ee6465f911671da31af49fd66252
Oliver Baker <avilebroker@gmail.com>
2020-02-12 14:33:02 -0600

    #5 Use Input instead of Output for the click function

--- packages/hs-angular-ui/src/app/button/button.component.ts
+++ packages/hs-angular-ui/src/app/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,7 +7,7 @@ import { Component, Input, Output } from '@angular/core';
 })
 
 export class ButtonComponent {
-  @Output()
+  @Input()
   onClick: CallableFunction;
 
   @Input()
