281fd3326c2155081682c834db58a56a2cb1f115
Oliver Baker <avilebroker@gmail.com>
2020-02-12 15:35:09 -0600

    #5 Change button selector

--- packages/hs-angular-ui/src/app/button/button.component.ts
+++ packages/hs-angular-ui/src/app/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-button',
+  selector: 'hs-ui-button',
   template: `
     <button
       class="button-container {{type}}"
