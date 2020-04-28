18d6c0a64df5187b5cf48968645516b638923393
Oliver <avilebroker@gmail.com>
2020-04-28 00:01:26 -0500

    Fix rollup error?

--- packages/hs-react-ui/index.js
+++ packages/hs-react-ui/index.js
@@ -1,7 +1,6 @@
 export { default as Button } from 'src/components/Button';
 export { default as Card } from 'src/components/Card';
 //# sourceMappingURL=index.js.map
-) && 'default' in ex) ? ex['default'] : ex; }
 
 var Button = _interopDefault(require('src/components/Button'));
 var Card = _interopDefault(require('src/components/Card'));
