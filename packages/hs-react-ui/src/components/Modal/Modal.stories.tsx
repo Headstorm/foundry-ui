f59c490525c239c5f7a8aea4d19418ba6c03db8c
jakeoien <joien@mail.smu.edu>
2020-05-13 01:09:09 -0500

    WIP - got modal sticky

--- packages/hs-react-ui/src/components/Modal/Modal.stories.tsx
+++ packages/hs-react-ui/src/components/Modal/Modal.stories.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import Modal from './Modal';
+import styled from 'styled-components';
 
 export default {
   title: 'Modal',
   component: Modal
 };
 
+const Background = styled.div`
+  background-image: url(https://upload.wikimedia.org/wikipedia/commons/b/b7/Chicago_cityscape_%285253757001%29.jpg);
+  background-size: cover;
+
+  height: 100vh;
+  width: 100vw;
+`
 export const Test = () => (
-  <Modal
-    header={<><span>Title</span><span>x</span></>}
-    body={'body'}
-    footer={'footer'} />
+  <Background>
+    <Modal
+      header={<><span>Title</span><span>x</span></>}
+      body={'body'}
+      footer={'footer'} />
+  </Background>
+
 );
--- packages/hs-react-ui/src/components/Modal/Modal.tsx
+++ packages/hs-react-ui/src/components/Modal/Modal.tsx
@@ -1,7 +1,11 @@
 import React, {FunctionComponent, ReactNode} from 'react';
 import Card from '../Card';
+import styled, {StyledComponentBase} from 'styled-components';
 
 export interface ModalProps {
+  StyledContainer?: String & StyledComponentBase<any, {}>,
+  StyledOverlay?: String & StyledComponentBase<any, {}>,
+
   onClickOutside?: () => void
   onClose?: () => void
   // todo consider making type for card parts so we can use the same type
@@ -13,7 +17,31 @@ export interface ModalProps {
   backgroundBlur?: number
   backgroundDarkness?: number
 }
+
+const ModalOverlay = styled.div`
+  height: 100%;
+  width: 100%;
+  
+  position: fixed;
+  top: 0;
+  left: 0;
+  
+  z-index: 1000;
+`
+
+const ModalContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+
+`
+
 const Modal: FunctionComponent<ModalProps> = ({
+  StyledContainer = ModalContainer,
+  StyledOverlay = ModalOverlay,
+
   onClickOutside,
   onClose,
 
@@ -23,13 +51,20 @@ const Modal: FunctionComponent<ModalProps> = ({
 
   backgroundBlur,
   backgroundDarkness
-}) => (
-  <Card
-    header={header}
-    footer={footer}
-  >
-    {body}
-  </Card>
-);
+}) => {
+  return (
+    <>
+      <StyledContainer>
+        <Card
+          header={header}
+          footer={footer}
+        >
+          {body}
+        </Card>
+      </StyledContainer>
+      <StyledOverlay />
+    </>
+  );
+}
 
 export default Modal;
