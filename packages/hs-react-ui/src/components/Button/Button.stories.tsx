2261b363817be9a47ce53de997cb771758caf3e9
Chloe Tedder <chloetedder@Chloes-Air.attlocal.net>
2020-05-14 14:26:36 -0500

    Remove buttonTypes as prop, add Icon option, alter stories

--- packages/hs-react-ui/src/components/Button/Button.stories.tsx
+++ packages/hs-react-ui/src/components/Button/Button.stories.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
 import { action } from '@storybook/addon-actions';
-import { select, text } from '@storybook/addon-knobs';
+import { select, text, boolean } from '@storybook/addon-knobs';
+import Icon from '@mdi/react';
+import { mdiLeadPencil } from '@mdi/js';
+import { ButtonContainers } from './ButtonContainers';
 
 import Button from './Button';
 import { ButtonTypes } from '../../enums/ButtonTypes';
@@ -16,7 +20,7 @@ export default {
 
 export const Basic = () => (
   <Button
-    buttonType={select('Button Type', ButtonTypes, ButtonTypes.primary)}
+    StyledContainer={ButtonContainers[select('Button Container', ButtonTypes, ButtonTypes.primary)]}
     onClick={action('button-click')}
   >
     {text('children', 'Default text')}
@@ -26,3 +30,41 @@ export const Basic = () => (
 Basic.story = {
   name: 'Basic'
 };
+
+// text container on figma, icons, icon yes/no and then send in icon
+const ThemedContainer = styled.button`
+  display: inline-block;
+
+  font-family: Helvetica;
+  font-size: 2em;
+  line-height: .3em;
+
+  padding: .75em .75em;
+  border-radius: 2em;
+
+  border: 0 none;
+
+  cursor: pointer;
+
+  background-color: rgb(51, 29, 138);
+
+  &:hover { background-color: rgb(51, 29, 138, .7); }
+  &:active { background-color: rgb(51, 29, 138, .3); }
+`;
+
+export const ThemedButton = () => {
+  const icon = (<Icon path={mdiLeadPencil}
+    size={1}
+    horizontal
+    vertical
+    rotate={90}/>)
+  return (
+    <Button
+      StyledContainer={ThemedContainer}
+      icon={boolean('Show icon?', true) ? icon : null}
+      onClick={action('button-click')}
+    >
+      {text('children', 'Edit')}
+    </Button>
+  );
+}
--- packages/hs-react-ui/src/components/Button/Button.tsx
+++ packages/hs-react-ui/src/components/Button/Button.tsx
@@ -1,77 +1,26 @@
-import React from 'react';
-import styled from 'styled-components';
-import { darken } from 'polished';
-
-import { ButtonTypes } from '../../enums/ButtonTypes';
-import colors from '../../constants/colors';
-
-const ButtonContainer = styled.button`
-  display: inline-block;
-
-  font-family: Gotham;
-  font-size: 1em;
-  line-height: 0;
-
-  padding: 1.25em 1em;
-  border-radius: 0.25em;
-
-  outline: 0 none;
-  border: 0 none;
-
-  cursor: pointer;
-
-  background-color: ${colors.grayXlight};
-  color: ${colors.grayDark};
-`;
-
-const PrimaryButtonContainer = styled(ButtonContainer)`
-  color: ${colors.background};
-  background-color: ${colors.primary};
-  &:hover { background-color: ${darken(.05, colors.primary)}; }
-  &:active { background-color: ${darken(.1, colors.primary)}; }
-`;
-
-const SecondaryButtonContainer = styled(ButtonContainer)`
-  color: ${colors.background};
-  background-color: ${colors.grayMedium};
-  &:hover { background-color: ${darken(.05, colors.grayMedium)}; }
-  &:active { background-color: ${darken(.1, colors.grayMedium)}; }
-`;
-
-const DestructiveButtonContainer = styled(ButtonContainer)`
-  color: ${colors.background};
-  background-color: ${colors.destructive};
-  &:hover { background-color: ${darken(.05, colors.destructive)}; }
-  &:active { background-color: ${darken(.1, colors.destructive)}; }
-`;
-
-const ButtonContainers = {
-  [ButtonTypes.default]: ButtonContainer,
-  [ButtonTypes.primary]: PrimaryButtonContainer,
-  [ButtonTypes.secondary]: SecondaryButtonContainer,
-  [ButtonTypes.destructive]: DestructiveButtonContainer,
-};
+import React, {ComponentType} from 'react';
+import styled, { StyledComponent } from 'styled-components';
+import { ButtonContainer } from './ButtonContainers';
 
 const Text = styled.span``;
 
-export type ButtonProps = {
-  buttonType: ButtonTypes,
-  children: string | Node,
+export interface ButtonProps {
+  StyledContainer?: StyledComponent<"button", any, {}>,
+  icon?: any,
+  children?: string | Node,
   onClick(): void
 };
 
+// put buttonType elsewhere
 const Button = ({
-  buttonType,
+  StyledContainer = ButtonContainer,
+  icon,
   children,
   onClick
-}: ButtonProps) => {
-  const Container = ButtonContainers[buttonType];
-
-  return (
-    <Container onClick={onClick}>
-      <Text>{children}</Text>
-    </Container>
-  )
-};
+}: ButtonProps) => (
+    <StyledContainer onClick={onClick}>
+      <Text>{children} {icon}</Text>
+    </StyledContainer>
+);
 
 export default Button;
