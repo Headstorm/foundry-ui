6e91a3182c1d28401d4bdd94e016b6683602f010
Oliver <avilebroker@gmail.com>
2020-05-22 12:15:44 -0500

    lint

--- packages/hs-react-ui/src/components/Label/Label.stories.tsx
+++ packages/hs-react-ui/src/components/Label/Label.stories.tsx
@@ -15,9 +15,13 @@ const design = {
   url: 'https://www.figma.com/file/3r2G00brulOwr9j7F6JF59/Generic-UI-Style?node-id=102%3A88',
 };
 
-storiesOf('Label', module)
-  .add(
-    'Basic Label',
-    () => <Label labelText={text('labelText', 'This is the label text')} color={select('Color', colors, colors.grayDark )}/>,
-    { design },
-  );
+storiesOf('Label', module).add(
+  'Basic Label',
+  () => (
+    <Label
+      labelText={text('labelText', 'This is the label text')}
+      color={select('Color', colors, colors.grayDark)}
+    />
+  ),
+  { design },
+);
