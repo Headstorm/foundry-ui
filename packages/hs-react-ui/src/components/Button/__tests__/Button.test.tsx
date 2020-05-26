b1824f0c2eb9ee8cf8f98bdf1f15d7ca4eb50283
jakeoien <joien@mail.smu.edu>
2020-05-26 00:02:50 -0500

    Add jest

---
+++ packages/hs-react-ui/src/components/Button/__tests__/Button.test.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+
+import Button from '../Button';
+
+describe('Button', () => {
+  it('fires click handler when clicked', () => {
+    const clickSpy = jest.fn();
+    const { getByText } = render(<Button onClick={clickSpy}>Click me</Button>);
+
+    fireEvent.click(getByText('Click me'));
+
+    expect(clickSpy).toHaveBeenCalled();
+  });
+
+  it('shows loading text when provided', async () => {
+    const { container, getByText } = render(
+      <Button isLoading={{ children: 'Loading...', icon: '⌛' }} onClick={() => {}} />,
+    );
+
+    await waitFor(() => getByText(/Loading.../));
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it('keeps the container the same when switching between isLoading and not isLoading', async () => {
+    const onClick = jest.fn();
+    const { getByText, rerender, asFragment } = render(
+      <Button isLoading={{ children: 'Loading...', icon: '⌛' }} onClick={onClick} />,
+    );
+
+    await waitFor(() => getByText(/Loading.../));
+    const loadingFragment = asFragment();
+
+    rerender(<Button onClick={onClick}>Submit</Button>);
+    await waitFor(() => getByText(/Submit/));
+    const loadedFragment = asFragment();
+
+    expect(loadingFragment).toMatchDiffSnapshot(loadedFragment);
+  });
+});
--- packages/hs-react-ui/src/setupTests.ts
+++ packages/hs-react-ui/src/setupTests.ts
@@ -4,3 +4,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+// .toMatchDiffSnapshot()
+import 'snapshot-diff/extend-expect';
+
+// utilities for cleaner snapshot diffs with styled components and .toHaveStyleRule() for expect
+import 'jest-styled-components';
