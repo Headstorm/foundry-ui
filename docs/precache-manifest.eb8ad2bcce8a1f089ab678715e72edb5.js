096dad97828d07e62bc17363cd876ae9d73982fc
Oliver <avilebroker@gmail.com>
2020-06-07 16:19:04 -0500

    build-storybook

---
+++ docs/precache-manifest.eb8ad2bcce8a1f089ab678715e72edb5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb37435b7ce63bcb2d8253f0174586b",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.5112a10156b5bfd97583.bundle.js"
+  },
+  {
+    "url": "runtime~main.5112a10156b5bfd97583.bundle.js"
+  },
+  {
+    "url": "vendors~main.5112a10156b5bfd97583.bundle.js"
+  },
+  {
+    "url": "vendors~main.5112a10156b5bfd97583.bundle.js.LICENSE.txt"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.2efeb4ee74f88d7a36e351c8491c66bd.js"
+  "precache-manifest.eb8ad2bcce8a1f089ab678715e72edb5.js"
 );
 
 self.addEventListener('message', (event) => {
