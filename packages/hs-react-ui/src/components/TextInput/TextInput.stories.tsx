e11357f4fb9bc594bb89a9686e5bfe4249086211
D.J. Brady <brady.donaldj@gmail.com>
2020-06-09 17:13:28 -0400

    35 get debounce working

--- packages/hs-react-ui/src/components/TextInput/TextInput.stories.tsx
+++ packages/hs-react-ui/src/components/TextInput/TextInput.stories.tsx
@@ -16,7 +16,7 @@ const design = {
   type: 'figma',
   url: 'https://www.figma.com/file/3r2G00brulOwr9j7F6JF59/Generic-UI-Style?node-id=102%3A29',
 };
-
+let debounceCounter = 0;
 storiesOf('TextInput', module)
   .add(
     'Basic Text Input',
@@ -27,36 +27,40 @@ storiesOf('TextInput', module)
         ...IconPaths,
       };
 
+
       return (
-        <>
-          <TextInput
-            ariaLabel='textInput'
-            onChange={(event) => {
-              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-              // @ts-ignore
-              const newValue = event.target.value;
-              setInputValue(newValue);
-              action('onChange')(newValue);
-            }}
-            value={inputValue}
-            placeholder={text('placeholder', 'Place Holder')}
-            onClear={
-              boolean('clearable', false)
-                ? () => {
-                    setInputValue('');
-                    action('onClear')();
-                  }
-                : undefined
-            }
-            iconPrefix={select('iconPrefix', options, options.none)}
-            isMultiline={boolean('isMultiline?', false)}
-            rows={number('rows', 0)}
-            cols={number('cols', 0)}
-            isValid={boolean('isValid', true)}
-            errorMessage={text('errorMessage', '')}
-            defaultValue={text('defaultValue', '')}
-          />
-        </>
+        <TextInput
+          ariaLabel="textInput"
+          onChange={event => {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            const newValue = event.target.value;
+            setInputValue(newValue);
+            action('onChange')(newValue);
+          }}
+          debounceInterval={1000}
+          debouncedOnChange={event => {
+            console.log(event);
+            action('debouncedOnChange')(++debounceCounter);
+          }}
+          value={inputValue}
+          placeholder={text('placeholder', 'Place Holder')}
+          onClear={
+            boolean('clearable', false)
+              ? () => {
+                  setInputValue('');
+                  action('onClear')();
+                }
+              : undefined
+          }
+          iconPrefix={select('iconPrefix', options, options.none)}
+          isMultiline={boolean('isMultiline?', false)}
+          rows={number('rows', 0)}
+          cols={number('cols', 0)}
+          isValid={boolean('isValid', true)}
+          errorMessage={text('errorMessage', '')}
+          defaultValue={text('defaultValue', '')}
+        />
       );
     },
     { design },
@@ -80,7 +84,7 @@ storiesOf('TextInput', module)
 
       return (
         <TextInput
-          onChange={(event) => {
+          onChange={event => {
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
             const newValue = event.target.value;
--- packages/hs-react-ui/src/components/TextInput/TextInput.tsx
+++ packages/hs-react-ui/src/components/TextInput/TextInput.tsx
@@ -2,12 +2,13 @@ import React, { ReactNode, SyntheticEvent } from 'react';
 import styled, { StyledComponentBase } from 'styled-components';
 import Icon from '@mdi/react';
 import { mdiClose } from '@mdi/js';
+import debounce from 'lodash.debounce';
 import fonts from '../../enums/fonts';
 import colors from '../../enums/colors';
 
 const Container = styled.div`
-${({isValid}: {isValid?: boolean}) => `
-  border 2px solid ${ isValid === false ? colors.destructive : colors.grayMedium};
+  ${({ isValid }: { isValid?: boolean }) => `
+  border 2px solid ${isValid === false ? colors.destructive : colors.grayMedium};
   min-width: 10rem;
   // width: 100%;
   position: relative;
@@ -67,6 +68,7 @@ export type TextInputProps = {
   iconPrefix?: string | ReactNode;
   onClear?: (event: SyntheticEvent) => void;
   onChange?: (event: SyntheticEvent) => void;
+  debouncedOnChange?: (event: SyntheticEvent) => void;
   onKeypress?: (event: SyntheticEvent) => void;
   onKeydown?: (event: SyntheticEvent) => void;
   onKeyup?: (event: SyntheticEvent) => void;
@@ -82,13 +84,16 @@ export type TextInputProps = {
   isMultiline?: boolean;
   errorMessage?: string;
   ariaLabel?: string;
+  debounceInterval?: number;
   StyledContainer?: string & StyledComponentBase<any, {}>;
   Input?: string & StyledComponentBase<any, {}>;
   StyledIconContainer?: string & StyledComponentBase<any, {}>;
 };
 
-const createIcon = (StyledIconContainer: string & StyledComponentBase<any, {}>,
-  iconPrefix: ReactNode) => {
+const createIcon = (
+  StyledIconContainer: string & StyledComponentBase<any, {}>,
+  iconPrefix: ReactNode,
+) => {
   if (typeof iconPrefix === 'string') {
     return (
       <StyledIconContainer>
@@ -100,12 +105,14 @@ const createIcon = (StyledIconContainer: string & StyledComponentBase<any, {}>,
   return <StyledIconContainer>{iconPrefix}</StyledIconContainer>;
 };
 
+
 const TextInput = ({
   id,
   placeholder = 'test props',
   iconPrefix,
   onClear,
-  onChange,
+  onChange = (e: SyntheticEvent) => {},
+  debouncedOnChange = () => {},
   onKeypress,
   onKeydown,
   onKeyup,
@@ -124,7 +131,11 @@ const TextInput = ({
   StyledContainer = Container,
   Input, // Not defaulting here due to the issue with <input as="textarea" />
   StyledIconContainer = IconContainer,
+  debounceInterval = 8,
 }: TextInputProps) => {
+
+  const debouncedChange = debounce(debouncedOnChange, debounceInterval);
+
   // Determine the correct input type. Using a single input and the 'as' keyword
   // to display as a text area disables the ability to set cols/rows
   let InputComponent: string & StyledComponentBase<any, {}> = TextInputContainer;
@@ -133,6 +144,8 @@ const TextInput = ({
   } else if (isMultiline) {
     InputComponent = TextAreaInputContainer;
   }
+
+  console.log()
   return (
     <StyledContainer isValid={isValid}>
       {iconPrefix && createIcon(StyledIconContainer, iconPrefix)}
@@ -143,16 +156,19 @@ const TextInput = ({
         rows={rows}
         aria-label={ariaLabel}
         placeholder={placeholder}
-        onChange={onChange}
-        onKeypress={onKeypress}
-        onKeydown={onKeydown}
-        onKeyup={onKeyup}
+        onChange={(e: SyntheticEvent) => {
+          e.persist();
+          onChange(e);
+          debouncedChange(e);
+        }}
+        onKeyPress={onKeypress}
+        onKeyDown={onKeydown}
+        onKeyUp={onKeyup}
         onFocus={onFocus}
         onBlur={onBlur}
         onReset={onReset}
         onInput={onInput}
-        value={value}
-        defaultValue={value ? undefined : defaultValue}
+        value={value ? value : defaultValue}
         id={id}
       />
       {onClear && value && (
@@ -162,7 +178,7 @@ const TextInput = ({
       )}
       {isValid === false && errorMessage && <ErrorContainer>{errorMessage}</ErrorContainer>}
     </StyledContainer>
-  )
+  );
 };
 
 TextInput.Container = Container;
