e048f27e69f8f1068e4d0c0908abdc53bdb9d23d
D.J. Brady <brady.donaldj@gmail.com>
2020-06-10 15:54:40 -0400

    HSUI-42 add interface for state

--- packages/hs-react-ui/src/components/Examples/Form.stories.tsx
+++ packages/hs-react-ui/src/components/Examples/Form.stories.tsx
@@ -20,19 +20,34 @@ const design = {
 };
 
 const stateAbbreviations = [
-  'AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA',
-  'MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT',
-  'VT','VA','WA','WV','WI','WY'
+  'AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL',
+  'GA','HI','ID','IL','IN','IA','KS','KY','LA','ME',
+  'MD','MA','MI','MN','MS','MO','MT','NE','NV','NH',
+  'NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI',
+  'SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'
  ];
 
-const defaultState = {
+ interface state {
+  firstName?: string;
+  lastName?: string;
+  title?: string;
+  city?: string;
+  streetAddress?: string;
+  state?: string;
+  phone?: string;
+  email?: string;
+  company?: string;
+  department?: string;
+  notifications?: boolean;
+ }
+
+const defaultState: state = {
   firstName: name.firstName(),
   lastName: name.lastName(),
   title: name.title(),
   city: address.city(),
   streetAddress: address.streetAddress(),
   state: address.stateAbbr(),
-  birthday: date.between("1/1/1950", "12/31/2000"),
   phone: phone.phoneNumber(),
   email: internet.email(),
   company: company.companyName(),
@@ -108,13 +123,13 @@ storiesOf('Form Example', module).add(
           labelText="First Name"
           htmlFor="firstName"
           isRequired
-          isValid={state.firstName.length > 0}
+          isValid={typeof state.firstName !== 'undefined' && state.firstName.length > 0}
           key="firstName"
         >
           <TextInput
             onChange={createCallback("firstName")}
             value={state.firstName}
-            isValid={state.firstName.length > 0}
+            isValid={typeof state.firstName !== 'undefined' && state.firstName.length > 0}
             errorMessage="First Name cannot be blank"
             id="firstName"
           />
@@ -124,13 +139,13 @@ storiesOf('Form Example', module).add(
           labelText="Last Name"
           htmlFor="lastName"
           isRequired
-          isValid={state.lastName.length > 0}
+          isValid={typeof state.lastName !== 'undefined' && state.lastName.length > 0}
           key="lastName"
         >
           <TextInput
             onChange={createCallback("lastName")}
             value={state.lastName}
-            isValid={state.lastName.length > 0}
+            isValid={typeof state.lastName !== 'undefined' && state.lastName.length > 0}
             errorMessage="Last Name cannot be blank"
             id="lastName"
           />
@@ -140,13 +155,13 @@ storiesOf('Form Example', module).add(
           labelText="Title"
           htmlFor="title"
           isRequired
-          isValid={state.title.length > 0}
+          isValid={typeof state.title !== 'undefined' && state.title.length > 0}
           key="title"
         >
           <TextInput
             onChange={createCallback("title")}
             value={state.title}
-            isValid={state.title.length > 0}
+            isValid={typeof state.title !== 'undefined' && state.title.length > 0}
             errorMessage="Title cannot be blank"
             id="title"
           />
@@ -155,7 +170,7 @@ storiesOf('Form Example', module).add(
         <Label
           labelText="Company"
           htmlFor="company"
-          isValid={state.company.length > 0}
+          isValid={typeof state.company !== 'undefined' && state.company.length > 0}
           key="company"
         >
           <TextInput
@@ -169,13 +184,13 @@ storiesOf('Form Example', module).add(
           labelText="Street Address"
           htmlFor="streetAddress"
           isRequired
-          isValid={state.streetAddress.length > 0}
+          isValid={typeof state.streetAddress !== 'undefined' && state.streetAddress.length > 0}
           key="streetAddress"
         >
           <TextInput
             onChange={createCallback("streetAddress")}
             value={state.streetAddress}
-            isValid={state.streetAddress.length > 0}
+            isValid={typeof state.streetAddress !== 'undefined' && state.streetAddress.length > 0}
             errorMessage="Street Address cannot be blank"
             id="streetAddress"
           />
@@ -185,11 +200,15 @@ storiesOf('Form Example', module).add(
           labelText="State"
           htmlFor="state"
           isRequired
-          isValid={state.state.length > 0}
+          isValid={typeof state.state !== 'undefined'}
           key="state"
         >
           <Dropdown
+            name="state-dropdown"
             options={stateAbbreviations}
+            color={colors.grayXlight}
+            values={[state.state as string]}
+            onSelect={(val) => { setState(Object.assign({}, { state: val as string })) }}
           />
         </Label>
       </Card>
