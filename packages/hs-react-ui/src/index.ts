5f994803007bafd032eceee754fca37df35b1fbc
Chloe Tedder <chloe_tedder@yahoo.com>
2020-06-15 12:00:14 -0500

    Adding to index label and text input

--- packages/hs-react-ui/src/index.ts
+++ packages/hs-react-ui/src/index.ts
@@ -3,9 +3,23 @@ import Card from './components/Card';
 import Checkbox from './components/Checkbox';
 import Divider from './components/Divider';
 import Dropdown from './components/Dropdown';
+import Label from './components/Label';
 import Modal from './components/Modal';
 import Table from './components/Table';
 import Text from './components/Text';
+import TextInput from './components/TextInput';
 import Progress from './components/Progress';
 
-export { Button, Card, Checkbox, Divider, Dropdown, Modal, Table, Text, Progress };
+export {
+  Button,
+  Card,
+  Checkbox,
+  Divider,
+  Dropdown,
+  Label,
+  Modal,
+  Table,
+  Text,
+  TextInput,
+  Progress
+};
