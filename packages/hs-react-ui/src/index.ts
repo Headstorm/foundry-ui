472a053fd1a81388fa0ce025d54dbbd58788e325
Chloe Tedder <chloe_tedder@yahoo.com>
2020-06-15 11:45:15 -0500

    Add dropdown as export

--- packages/hs-react-ui/src/index.ts
+++ packages/hs-react-ui/src/index.ts
@@ -2,9 +2,10 @@ import Button from './components/Button';
 import Card from './components/Card';
 import Checkbox from './components/Checkbox';
 import Divider from './components/Divider';
+import Dropdown from './components/Dropdown';
 import Modal from './components/Modal';
 import Table from './components/Table';
 import Text from './components/Text';
 import Progress from './components/Progress';
 
-export { Button, Card, Checkbox, Divider, Modal, Table, Text, Progress };
+export { Button, Card, Checkbox, Divider, Dropdown, Modal, Table, Text, Progress };
