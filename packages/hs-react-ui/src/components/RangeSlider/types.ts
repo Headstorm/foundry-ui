195dfdc77599a14dedc2e341a92be8ee76cc1b5f
Oliver <avilebroker@gmail.com>
2020-06-16 19:23:32 -0500

    #32 type and remove package lock

--- packages/hs-react-ui/src/components/RangeSlider/types.ts
+++ packages/hs-react-ui/src/components/RangeSlider/types.ts
@@ -37,10 +37,10 @@ export type RangeSliderProps = {
   springOnRelease?: boolean;
   debounceInterval?: number;
   axisLock?: 'x' | 'y' | '';
-  onDrag?: (val) => void;
+  onDrag?: (val: number) => void;
   disabled?: boolean;
   min: number;
   max: number;
   values?: number[] | valueProp[];
-  testId: string;
+  testId?: string;
 };
