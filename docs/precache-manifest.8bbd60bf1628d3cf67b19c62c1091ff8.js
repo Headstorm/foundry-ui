cceca6b3a68e115245d1708c8d163e40785965b5
Oliver <avilebroker@gmail.com>
2020-06-16 21:43:53 -0500

    Update storybook addons

---
+++ docs/precache-manifest.8bbd60bf1628d3cf67b19c62c1091ff8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c45ded3122a1608fe94a86fda5779975",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.50e40473f8c4036d0076.bundle.js"
+  },
+  {
+    "url": "runtime~main.50e40473f8c4036d0076.bundle.js"
+  },
+  {
+    "url": "vendors~main.50e40473f8c4036d0076.bundle.js"
+  },
+  {
+    "url": "vendors~main.50e40473f8c4036d0076.bundle.js.LICENSE.txt"
+  }
+]);
