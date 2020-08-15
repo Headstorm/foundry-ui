18efdd5afa2431c095c93a96e31591eb7035e514
Will <wmangimelli@headstorm.com>
2020-08-14 21:20:47 -0500

    Add jest-coverage-badges module

--- packages/hs-react-ui/jest.config.js
+++ packages/hs-react-ui/jest.config.js
@@ -36,12 +36,13 @@ module.exports = {
   // ],
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   'json',
-  //   'text',
-  //   'lcov',
-  //   'clover'
-  // ],
+  coverageReporters: [
+    'json-summary',
+    'text',
+    'lcov',
+    'clover',
+    'html',
+  ],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
