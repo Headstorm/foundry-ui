699f6f0b1d8e4a68a79c5018a0b14a3a5e17e16c
Will <wmangimelli@headstorm.com>
2020-08-20 23:05:36 -0500

    Remove native HTML attributes from api and replace with spread to input element

--- packages/hs-react-ui/src/components/TextInput/TextInput.stories.tsx
+++ packages/hs-react-ui/src/components/TextInput/TextInput.stories.tsx
@@ -1,8 +1,6 @@
 import React, { useState, useCallback } from 'react';
-import styled from 'styled-components';
 import { text, select, boolean, number } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
-import Icon from '@mdi/react';
 import * as IconPaths from '@mdi/js';
 import { storiesOf } from '@storybook/react';
 
@@ -12,117 +10,88 @@ const design = {
   type: 'figma',
   url: 'https://www.figma.com/file/3r2G00brulOwr9j7F6JF59/Generic-UI-Style?node-id=102%3A29',
 };
-storiesOf('TextInput', module)
-  .add(
-    'Basic Text Input',
-    () => {
-      const [inputValue, setInputValue] = useState('');
-      const options = {
-        none: '',
-        ...IconPaths,
-      };
+storiesOf('TextInput', module).add(
+  'Basic Text Input',
+  () => {
+    const [inputValue, setInputValue] = useState('');
+    const options = {
+      none: '',
+      ...IconPaths,
+    };
 
-      // Setup callbacks to prevent unnecessary rendering
-      const onChangeCallback = useCallback(event => {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        const newValue = event.target.value;
-        setInputValue(newValue);
-        action('onChange')(newValue);
-      }, []);
-      const onDebounceCallback = useCallback(event => {
-        action('onDebounceCallback')(event.target.value);
-      }, []);
-      const onClearCallback = useCallback(() => {
-        setInputValue('');
-        action('onClear')();
-      }, []);
-      const onFocusCallback = useCallback(event => {
-        action('onFocusCallback')(event.target.value);
-      }, []);
-      const onBlurCallback = useCallback(event => {
-        action('onBlurCallback')(event.target.value);
-      }, []);
-      const onInputCallback = useCallback(event => {
-        action('onInputCallback')(event.target.value);
-      }, []);
-      const onKeyPressCallback = useCallback(event => {
-        action('onKeypressCallback')(event.key);
-      }, []);
-      const onKeyDownCallback = useCallback(event => {
-        action('onKeyDownCallback')(event.key);
-      }, []);
-      const onKeyUpCallback = useCallback(event => {
-        action('onKeyUpCallback')(event.key);
-      }, []);
+    // Setup callbacks to prevent unnecessary rendering
+    const onChangeCallback = useCallback(event => {
+      const newValue = event.target.value;
+      setInputValue(newValue);
+      action('onChange')(newValue);
+    }, []);
+    const onDebounceCallback = useCallback(event => {
+      action('onDebounceCallback')(event.target.value);
+    }, []);
+    const onClearCallback = useCallback(() => {
+      setInputValue('');
+      action('onClear')();
+    }, []);
+    const onFocusCallback = useCallback(event => {
+      action('onFocusCallback')(event.target.value);
+    }, []);
+    const onBlurCallback = useCallback(event => {
+      action('onBlurCallback')(event.target.value);
+    }, []);
+    const onInputCallback = useCallback(event => {
+      action('onInputCallback')(event.target.value);
+    }, []);
+    const onKeyPressCallback = useCallback(event => {
+      action('onKeypressCallback')(event.key);
+    }, []);
+    const onKeyDownCallback = useCallback(event => {
+      action('onKeyDownCallback')(event.key);
+    }, []);
+    const onKeyUpCallback = useCallback(event => {
+      action('onKeyUpCallback')(event.key);
+    }, []);
 
-      return (
-        <TextInput
-          ariaLabel={text('ariaLabel', 'textInput')}
-          onChange={onChangeCallback}
-          debounceInterval={number('debounceInterval', 150)}
-          debouncedOnChange={onDebounceCallback}
-          value={inputValue}
-          placeholder={text('placeholder', 'Place Holder')}
-          onClear={boolean('clearable', false) ? onClearCallback : undefined}
-          iconPrefix={select('iconPrefix', options, options.none)}
-          isMultiline={boolean('isMultiline?', false)}
-          rows={number('rows', 0)}
-          cols={number('cols', 0)}
-          isValid={boolean('isValid', true)}
-          errorMessage={text('errorMessage', '')}
-          defaultValue={text('defaultValue', '')}
-          type={text('type', '')}
-          onInput={onInputCallback}
-          onKeyPress={onKeyPressCallback}
-          onKeyDown={onKeyDownCallback}
-          onKeyUp={onKeyUpCallback}
-          onFocus={onFocusCallback}
-          onBlur={onBlurCallback}
-          multiLineIsResizable={boolean('multiLineIsResizable', false)}
-        />
-      );
-    },
-    { design },
-  )
-  .add(
-    'Text Input with all the gadgets',
-    () => {
-      const Input = styled(TextInput.Input)`
-        width: 300px;
-        height: 80px;
-      `;
-      const [inputValue, setInputValue] = useState('');
-      const options = {
-        none: '',
-        ...IconPaths,
-      };
-      const getIconPath = (path: string) => (path ? <Icon size="16px" path={path} /> : undefined);
-      const isMultiline = inputValue.length > 15;
-      const isError =
-        inputValue.length < 5 && inputValue.length > 0 ? 'Short Message Error' : undefined;
+    const generalGroup = 'General';
+    const multilineGroup = 'Multiline';
+    const errorMessageGroup = 'Error messages';
+    const debounceGroup = 'Debounced';
+    const characterCountGroup = 'Character count';
 
-      return (
-        <TextInput
-          onChange={event => {
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            const newValue = event.target.value;
-            setInputValue(newValue);
-            action('onChange')(newValue);
-          }}
-          value={inputValue}
-          placeholder={text('placeholder', 'Place Holder')}
-          onClear={() => {
-            setInputValue('');
-            action('onClear')();
-          }}
-          iconPrefix={getIconPath(select('iconPrefix', options, options.mdiComment))}
-          isMultiline={isMultiline}
-          errorMessage={isError}
-          StyledInput={Input}
-        />
-      );
-    },
-    { design },
-  );
+    return (
+      <TextInput
+        aria-label={text('ariaLabel', 'textInput', generalGroup)}
+        onChange={onChangeCallback}
+        debounceInterval={number('debounceInterval', 150, undefined, debounceGroup)}
+        debouncedOnChange={onDebounceCallback}
+        disabled={boolean('disabled', false, generalGroup)}
+        value={inputValue}
+        placeholder={text('placeholder', 'Placeholder', generalGroup)}
+        onClear={boolean('clearable', false, generalGroup) ? onClearCallback : undefined}
+        iconPrefix={select('iconPrefix', options, options.none, generalGroup)}
+        isMultiline={boolean('isMultiline?', false, multilineGroup)}
+        rows={number('rows', 0, undefined, multilineGroup)}
+        cols={number('cols', 0, undefined, multilineGroup)}
+        isValid={boolean('isValid', true, errorMessageGroup)}
+        errorMessage={text('errorMessage', '', errorMessageGroup)}
+        defaultValue={text('defaultValue', '', generalGroup)}
+        type={text('type', '', generalGroup)}
+        onInput={onInputCallback}
+        onKeyPress={onKeyPressCallback}
+        onKeyDown={onKeyDownCallback}
+        onKeyUp={onKeyUpCallback}
+        onFocus={onFocusCallback}
+        onBlur={onBlurCallback}
+        multiLineIsResizable={boolean('multiLineIsResizable', false, multilineGroup)}
+        showCharacterCount={boolean('showCharacterCount', true, characterCountGroup)}
+        maxLength={select(
+          'maxLength',
+          { 5: 5, 20: 20, 100: 100, none: undefined },
+          20,
+          characterCountGroup,
+        )}
+        allowTextBeyondMaxLength={boolean('allowTextBeyondMaxLength', false, characterCountGroup)}
+      />
+    );
+  },
+  { design },
+);
--- packages/hs-react-ui/src/components/TextInput/TextInput.tsx
+++ packages/hs-react-ui/src/components/TextInput/TextInput.tsx
@@ -1,93 +1,131 @@
-import React, { ReactNode, SyntheticEvent, useCallback } from 'react';
-import styled, { StyledComponentBase } from 'styled-components';
+import React, {
+  ChangeEvent,
+  EventHandler,
+  ReactNode,
+  SyntheticEvent,
+  useCallback,
+  TextareaHTMLAttributes,
+  InputHTMLAttributes,
+} from 'react';
+import styled, { css, StyledComponentBase } from 'styled-components';
 import Icon from '@mdi/react';
 import { mdiClose } from '@mdi/js';
 import debounce from 'lodash.debounce';
-import colors from '../../enums/colors';
-import { Div, TextArea, Input as InputElement } from '../../htmlElements';
+import { Div, Input as InputElement, TextArea } from '../../htmlElements';
+import { SubcomponentPropsType } from '../commonTypes';
+import { useTheme } from '../../context';
+import { disabledStyles } from '../../utils/color';
 
 const Container = styled(Div)`
-  ${({ isValid }: { isValid?: boolean }) => `
-    border 2px solid ${isValid === false ? colors.destructive : colors.grayMedium};
-    min-width: 10rem;
-    position: relative;
-    display: flex;
-    flex-flow: row;
-    border-radius: 0.25em;
-    background-color: ${colors.background};
-  `}
+  ${({ disabled = false, isValid }: { disabled?: boolean; isValid?: boolean }) => {
+    const { colors } = useTheme();
+    return `
+      border 2px solid ${isValid === false ? colors.destructive : colors.grayMedium};
+      min-width: 10rem;
+      position: relative;
+      display: flex;
+      flex-flow: row;
+      border-radius: 0.25em;
+      background-color: ${colors.background};
+      ${disabled ? disabledStyles() : ''}
+  `;
+  }}
 `;
 
 const TextInputContainer = styled(InputElement)`
-  border: 0 none;
-  flex-grow: 1;
-  outline: 0 none;
-  height: 2em;
-  font-size: 1em;
-  padding: 0.5rem;
-  background-color: ${colors.background};
+  ${() => {
+    const { colors } = useTheme();
+    return `
+      border: 0 none;
+      flex-grow: 1;
+      outline: 0 none;
+      height: 2em;
+      font-size: 1em;
+      padding: 0.5rem;
+      background-color: ${colors.transparent};
+  `;
+  }}
 `;
 
 const TextAreaInputContainer = styled(TextArea)`
-  ${({ multiLineIsResizable }: TextInputProps) => `
-    border: 0 none;
-    flex-grow: 1;
-    outline: 0 none;
-    font-size: 1em;
-    min-height: 2em;
-    min-width: 0px;
-    padding: .5rem;
-    background-color: ${colors.background};
-    resize: ${multiLineIsResizable ? 'both' : 'none'};
-  `}
+  ${({ multiLineIsResizable }: TextInputProps) => {
+    const { colors } = useTheme();
+    return `
+      border: 0 none;
+      flex-grow: 1;
+      outline: 0 none;
+      font-size: 1em;
+      min-height: 2em;
+      min-width: 0px;
+      padding: .5rem;
+      background-color: ${colors.transparent};
+      resize: ${multiLineIsResizable ? 'both' : 'none'};
+    `;
+  }}
 `;
 
 const IconContainer = styled(Div)`
-  padding: 0.5em;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: ${colors.grayMedium};
-  cursor: pointer;
+  ${() => {
+    const { colors } = useTheme();
+    return `
+      padding: 0.5em;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: ${colors.grayMedium};
+      cursor: pointer;
+    `;
+  }}
+`;
+
+const CharacterCounter = styled(Div)`
+  ${({ textIsTooLong, isValid, errorMessage }) => {
+    const { colors } = useTheme();
+    return css`
+      position: absolute;
+      top: calc(100% + ${isValid && (!errorMessage || errorMessage !== '') ? '0.25em' : '2em'});
+      right: 0.25em;
+      color: ${textIsTooLong ? colors.destructive : colors.grayLight};
+    `;
+  }}
 `;
 
 const ErrorContainer = styled(Div)`
-  position: absolute;
-  top: calc(100% + 0.25em);
-  color: ${colors.destructive};
-  font-size: 0.75rem;
+  ${() => {
+    const { colors } = useTheme();
+    return css`
+      position: absolute;
+      top: calc(100% + 0.25em);
+      color: ${colors.destructive};
+      font-size: 0.75rem;
+    `;
+  }}
 `;
 
-export type TextInputProps = {
-  id?: string;
-  placeholder?: string;
-  iconPrefix?: string | ReactNode;
-  onClear?: (event: SyntheticEvent) => void;
-  onChange?: (event: SyntheticEvent) => void;
-  debouncedOnChange?: (event: SyntheticEvent) => void;
-  onKeyPress?: (event: SyntheticEvent) => void;
-  onKeyDown?: (event: SyntheticEvent) => void;
-  onKeyUp?: (event: SyntheticEvent) => void;
-  onBlur?: (event: SyntheticEvent) => void;
-  onInput?: (event: SyntheticEvent) => void;
-  onFocus?: (event: SyntheticEvent) => void;
-  onReset?: (event: SyntheticEvent) => void;
-  cols?: number;
-  rows?: number;
-  value?: string;
-  defaultValue?: string;
-  isValid?: boolean;
-  isMultiline?: boolean;
-  errorMessage?: string;
-  ariaLabel?: string;
-  type?: string;
-  debounceInterval?: number;
-  multiLineIsResizable?: boolean;
-  StyledContainer?: string & StyledComponentBase<any, {}>;
-  StyledInput?: string & StyledComponentBase<any, {}>;
-  StyledIconContainer?: string & StyledComponentBase<any, {}>;
-};
+export type TextInputProps = InputHTMLAttributes<HTMLInputElement> &
+  TextareaHTMLAttributes<HTMLTextAreaElement> & {
+    iconPrefix?: string | ReactNode;
+    onClear?: (event: SyntheticEvent) => void;
+    debouncedOnChange?: EventHandler<ChangeEvent<HTMLInputElement>>;
+    isValid?: boolean;
+    isMultiline?: boolean;
+    errorMessage?: string;
+    debounceInterval?: number;
+    multiLineIsResizable?: boolean;
+    maxLength?: number;
+    allowTextBeyondMaxLength?: boolean;
+    showCharacterCount?: boolean;
+
+    StyledContainer?: string & StyledComponentBase<any, {}>;
+    StyledInput?: string & StyledComponentBase<any, {}>;
+    StyledIconContainer?: string & StyledComponentBase<any, {}>;
+    StyledErrorContainer?: string & StyledComponentBase<any, {}>;
+    containerProps?: SubcomponentPropsType;
+    inputProps?: SubcomponentPropsType;
+    iconContainerProps?: SubcomponentPropsType;
+    errorContainerProps?: SubcomponentPropsType;
+  };
 
 const createIcon = (
   StyledIconContainer: string & StyledComponentBase<any, {}>,
@@ -96,7 +134,7 @@ const createIcon = (
   if (typeof iconPrefix === 'string') {
     return (
       <StyledIconContainer>
-        <Icon size="16px" path={iconPrefix} />
+        <Icon size="1rem" path={iconPrefix} />
       </StyledIconContainer>
     );
   }
@@ -107,36 +145,40 @@ const createIcon = (
 const defaultCallback = () => {}; // eslint-disable-line @typescript-eslint/no-empty-function
 
 const TextInput = ({
-  id,
-  placeholder,
-  iconPrefix,
-  onClear,
-  onChange = defaultCallback,
-  debouncedOnChange = defaultCallback,
-  onKeyPress,
-  onKeyDown,
-  onKeyUp,
-  onBlur,
-  onInput,
-  onFocus,
-  onReset,
+  // Destructure native HTML attributes to provide default values
+  defaultValue = '',
+  type = 'text',
+  disabled = false,
   cols = 10,
   rows = 10,
-  value,
-  defaultValue,
-  isValid,
+
+  debouncedOnChange = defaultCallback,
+  onClear,
+  iconPrefix,
+  isValid = true,
   isMultiline,
   errorMessage,
-  ariaLabel,
-  type = 'text',
-  StyledContainer = Container,
-  StyledInput, // Not defaulting here due to the issue with <input as="textarea" />
-  StyledIconContainer = IconContainer,
   debounceInterval = 8,
   multiLineIsResizable,
-}: TextInputProps) => {
+  maxLength,
+  allowTextBeyondMaxLength = false,
+  showCharacterCount = false,
+
+  StyledContainer = Container,
+  StyledInput,
+  StyledIconContainer = IconContainer,
+  StyledErrorContainer = ErrorContainer,
+  containerProps = {},
+  inputProps = {},
+  iconContainerProps = {},
+  errorContainerProps = {},
+  ...nativeHTMLAttributes
+}: TextInputProps): JSX.Element => {
   // Debounce the change function using useCallback so that the function is not initialized each time it renders
-  const debouncedChange = useCallback(debounce(debouncedOnChange, debounceInterval), []);
+  const debouncedChange = useCallback(debounce(debouncedOnChange, debounceInterval), [
+    debouncedOnChange,
+    debounceInterval,
+  ]);
 
   // Determine the correct input type. Using a single input and the 'as' keyword
   // to display as a text area disables the ability to set cols/rows
@@ -146,45 +188,56 @@ const TextInput = ({
   } else if (isMultiline) {
     InputComponent = TextAreaInputContainer;
   }
+  const displayValue = nativeHTMLAttributes.value || defaultValue;
 
   return (
-    <StyledContainer isValid={isValid}>
+    <StyledContainer disabled={disabled} isValid={isValid} {...containerProps}>
       {iconPrefix && createIcon(StyledIconContainer, iconPrefix)}
-      {/*
-        // @ts-ignore */}
       <InputComponent
+        value={displayValue}
+        type={type}
+        disabled={disabled}
         cols={cols}
         rows={rows}
-        aria-label={ariaLabel}
-        placeholder={placeholder}
-        onChange={(e: SyntheticEvent) => {
+        onChange={(e: ChangeEvent<HTMLInputElement>) => {
           e.persist();
-          onChange(e);
+          if (maxLength && maxLength >= 0) {
+            e.target.value = allowTextBeyondMaxLength
+              ? e.target.value
+              : e.target.value.slice(0, maxLength);
+          }
+          if (nativeHTMLAttributes.onChange) {
+            nativeHTMLAttributes.onChange(e);
+          }
           debouncedChange(e);
         }}
-        onKeyPress={onKeyPress}
-        onKeyDown={onKeyDown}
-        onKeyUp={onKeyUp}
-        onFocus={onFocus}
-        onBlur={onBlur}
-        onReset={onReset}
-        onInput={onInput}
-        value={value || defaultValue}
-        id={id}
-        type={type}
         multiLineIsResizable={multiLineIsResizable}
+        {...inputProps}
+        {...nativeHTMLAttributes}
       />
       {onClear && (
-        <StyledIconContainer onClick={onClear}>
+        <StyledIconContainer onClick={onClear} {...iconContainerProps}>
           <Icon path={mdiClose} size="1em" />
         </StyledIconContainer>
       )}
-      {isValid === false && errorMessage && <ErrorContainer>{errorMessage}</ErrorContainer>}
+      {showCharacterCount && maxLength && (
+        <CharacterCounter
+          errorMessage={errorMessage}
+          isValid={isValid}
+          textIsTooLong={(displayValue as string).length > maxLength}
+        >
+          {(displayValue as string).length} / {maxLength}
+        </CharacterCounter>
+      )}
+      {isValid === false && errorMessage && (
+        <StyledErrorContainer {...errorContainerProps}>{errorMessage}</StyledErrorContainer>
+      )}
     </StyledContainer>
   );
 };
 
 TextInput.Container = Container;
+TextInput.ErrorContainer = ErrorContainer;
 TextInput.Input = TextInputContainer;
 TextInput.IconContainer = IconContainer;
 TextInput.TextArea = TextAreaInputContainer;
