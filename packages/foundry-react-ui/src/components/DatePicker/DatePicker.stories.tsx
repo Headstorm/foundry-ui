9707b0ea7f9122fd36c459c3bfc92f58d45f86c4
Oliver Baker <avilebroker@gmail.com>
2020-12-18 12:13:22 -0600

    Month dropdown

--- packages/foundry-react-ui/src/components/DatePicker/DatePicker.stories.tsx
+++ packages/foundry-react-ui/src/components/DatePicker/DatePicker.stories.tsx
@@ -8,7 +8,7 @@ import DatePicker from './DatePicker';
 
 const design = {
   type: 'figma',
-  url: 'https://www.figma.com/file/3r2G00brulOwr9j7F6JF59/Generic-UI-Style?node-id=102%3A28',
+  url: 'https://www.figma.com/file/3r2G00brulOwr9j7F6JF59/Foundry-UI?node-id=105%3A6',
 };
 
 storiesOf('DatePicker', module)
--- packages/foundry-react-ui/src/components/DatePicker/DatePicker.tsx
+++ packages/foundry-react-ui/src/components/DatePicker/DatePicker.tsx
@@ -1,7 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const DatePicker = (): JSX.Element | null => {
-  return <div>Today's Date</div>;
+import variants from '../../enums/variants';
+import Dropdown, { objectOrArrayToDropdownOptions } from '../Dropdown';
+import Card from '../Card';
+
+const weekends = [0, 6];
+const weekdays = [1, 2, 3, 4, 5];
+const daysByMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const daysByMonthOnLeapYear = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const shortDayNames = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
+const longDayNames = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const shortMonthNames = [
+  'JAN',
+  'FEB',
+  'MAR',
+  'APR',
+  'MAY',
+  'JUN',
+  'JUL',
+  'AUG',
+  'SEP',
+  'OCT',
+  'NOV',
+  'DEC',
+];
+const longMonthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+const monthOptions = objectOrArrayToDropdownOptions(longMonthNames);
+const today = new Date();
+
+export const isLeapYear = (date: Date): boolean => {
+  const year = date.getFullYear();
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+};
+
+export type DatePickerProps = {
+  selectedDate?: Date; // the currently selected date
+  onSelect?: () => void; // onClick of a date in the valid date range
+  initialMonthView?: number; // which month view is open on mount (default is the current month)
+  initialYearView?: number; // which year view is open on mount (default is the current year)
+};
+
+// TODO: Render the current month/year as dropdowns
+// TODO: Render a month of days in a grid with blanks
+
+const DatePicker = ({
+  initialMonthView = today.getMonth(),
+  initialYearView = today.getFullYear(),
+}: DatePickerProps): JSX.Element | null => {
+  const [monthView, setMonthView] = useState(`${initialMonthView}`);
+  const [yearView, setYearView] = useState(initialYearView);
+
+  return (
+    <Card>
+      <Dropdown
+        variant={variants.text}
+        options={monthOptions}
+        values={[monthView]}
+        onSelect={months => setMonthView(months[0])}
+      />
+      <Dropdown
+        variant={variants.text}
+        placeholder={`${yearView}`}
+        onSelect={year => setYearView(year)}
+      />
+    </Card>
+  );
 };
 
 export default DatePicker;
