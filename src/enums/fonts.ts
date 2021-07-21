9e7ffff6c755040bd81cb4bda686cee314721c60
Oliver Baker <avilebroker@gmail.com>
2021-07-21 15:57:23 -0500

    feat(globalstyles): reduce load times by using Arial instead of Montserrat
    Previously, Montserrat was used on every foundry-ui component by default. It could be overwritten by
    another using our globalStyles api, but the font would still be loaded by Google Fonts. This change
    removes Montserrat and its loading in favor of an OS-default font: Arial. It also looks better and
    Headstorm doesn't use Montserrat anymore anyway.
    
    complete #250

--- src/enums/fonts.ts
+++ src/enums/fonts.ts
@@ -1,6 +1,5 @@
 enum fonts {
-  importFonts = "@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;700;900&display=swap');",
-  body = 'font-family: Montserrat, Roboto, sans-serif;',
+  body = 'font-family: Arial, Roboto, sans-serif;',
 }
 
 export default fonts;
