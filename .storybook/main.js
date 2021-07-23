5d2b50697a8ff122c0d9efd7da97aab4bf45337c
Dawson Booth <github@dawsonbooth.com>
2021-07-23 13:54:36 -0500

    Remove current stories from glob discovery

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.new.mdx', '../src/**/*.stories.new.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
