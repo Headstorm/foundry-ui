ace60339f0ca90d48a3f1a315904046b1c9cfe26
Dawson Booth <github@dawsonbooth.com>
2021-07-24 14:18:53 -0500

    Add updated storybook addons and configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,11 @@
 module.exports = {
   // stories: ['../src/components/**/*.stories.tsx', '../src/**/*.stories.@(js|mdx)'],
-  stories: ['../src/components/Appendix/*.stories.tsx'],
+  stories: ['../src/components/Appendix/*.stories.tsx', '../src/components/Button/*.stories.tsx'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
-    '@storybook/addon-storysource',
     'storybook-addon-designs',
+    '@storybook/addon-a11y',
   ],
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,24 @@
+import { withDesign } from 'storybook-addon-designs';
+import { withA11y } from '@storybook/addon-a11y';
+
+import colors from 'src/enums/colors';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+  layout: 'centered',
+  backgrounds: {
+    values: [
+      { name: 'grayDark', value: colors.grayDark },
+      { name: 'grayLight', value: colors.grayLight },
+      { name: 'background', value: colors.background, default: true },
+    ],
+  },
+};
+
+export const decorators = [withDesign, withA11y];
