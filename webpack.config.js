f0784172122693205fce0820fe6f227f81827c22
Dawson Booth <github@dawsonbooth.com>
2021-08-04 10:04:04 -0500

    #128 Format webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,38 +8,38 @@ module.exports = {
     path: path.join(__dirname, '/dist'),
     filename: 'main.js',
     publicPath: '/',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
   },
   devtool: 'source-map',
   externals: {
-    "styled-components": "styled-components",
-    "react": "react",
-    "react-dom": "react-dom"
+    'styled-components': 'styled-components',
+    react: 'react',
+    'react-dom': 'react-dom',
   },
   // adding .ts and .tsx to resolve.extensions will help babel look for .ts and .tsx files to transpile
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', 'jsx']
+    extensions: ['.ts', '.tsx', '.js', 'jsx'],
   },
   module: {
     rules: [
       // we use babel-loader to load our jsx and tsx files
-    {
-      test: /\.(ts|js)x?$/,
-      exclude: /(node_modules)/,
-      use: {
-        loader: 'babel-loader'
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
-    },
-    // css-loader to bundle all the css files into one file and style-loader to add all the styles  inside the style tag of the document
-    {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader']
-    },
-    {
-      test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
-      exclude: /node_modules/,
-      use: ['file-loader?name=[name].[ext]'] // ?name=[name].[ext] is only necessary to preserve the original file name
-    }
-  ]
-},
+      // css-loader to bundle all the css files into one file and style-loader to add all the styles  inside the style tag of the document
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+        exclude: /node_modules/,
+        use: ['file-loader?name=[name].[ext]'], // ?name=[name].[ext] is only necessary to preserve the original file name
+      },
+    ],
+  },
 };
