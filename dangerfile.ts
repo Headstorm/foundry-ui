7b992ebe9102d804be47703ab6e65670fb830b1f
nathan <nathanswesley@gmail.com>
2021-08-04 14:22:28 -0500

    ci: deleted files check
    #247

--- dangerfile.ts
+++ dangerfile.ts
@@ -2,3 +2,6 @@ import {message, danger} from "danger"
 
 const modifiedMD = danger.git.modified_files.join("- ")
 message("Changed Files in this PR: \n - " + modifiedMD)
+
+const deletedFiles = danger.git.deleted_files.join("- ")
+message("Deleted Files in this PR: \n - " + deletedFiles)
