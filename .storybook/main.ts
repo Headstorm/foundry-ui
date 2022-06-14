4fc322fa0e0da0a8b9995a02066bccc57d18b42f
Dawson Booth <dbooth@headstorm.com>
2022-06-14 16:36:31 -0500

    Upgrade storybook to use webpack 5

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,6 @@ module.exports = {
   stories: ['../src/components/**/*.stories.tsx', '../src/**/*.stories.@(js|mdx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app', 'storybook-addon-designs', '@storybook/addon-a11y'],
   core: {
-    builder: "webpack4"
+    builder: "webpack5"
   }
 };
