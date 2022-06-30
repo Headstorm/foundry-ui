8f88a2fd6e81cb5dbe566f4561fa095c8d4cfd00
Oliver Baker <avilebroker@gmail.com>
2022-06-30 12:53:48 -0500

    use regular translucent color value

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -22,7 +22,7 @@ const Underlay = styled(AnimatedDiv)`
   height: 100%;
   width: 100%;
 
-  background-color: transparentize(0.5, '#000');
+  background-color: rgba(0, 0, 0, 0.5);
 
   position: fixed;
   top: 0;
