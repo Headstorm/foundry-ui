a529bd02d894a6dc7dc17bd3e8e0c901f0dd9de8
Xavier Donnellon <xavier.donnellon@gmail.com>
2023-06-16 11:04:14 -0500

    Add support component builder types for Table

--- src/components/commonTypes.ts
+++ src/components/commonTypes.ts
@@ -1,5 +1,9 @@
 import { StyledComponentBase } from 'styled-components';
 
 export type SubcomponentPropsType = Record<string, unknown>;
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type StyledSubcomponentType = string & StyledComponentBase<any, SubcomponentPropsType>;
+export type StyledSubcomponentType = React.FC | ((props: any) => JSX.Element) | StyledComponent;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type StyledComponent = string & StyledComponentBase<any, SubcomponentPropsType>;
