ccb623af6661df266f402f46f6781d6c3dbeaa2f
Oliver Baker <avilebroker@gmail.com>
2023-10-24 14:44:03 -0500

    Fixes #481 Spotlight | Custom scroll targets (#482)
    * Add auto-detect and custom scroll options
    
    * Fix parent identification
    
    * fix(spotlight, usescrollobserver): automatically scroll nearest scrollable element
    
    Instead of always trying to scroll the window to highlight the target, it now finds the nearest
    scrollable element and scrolls that instead. Many times, that will be the window/html element
    
    fixes #481
    
    * snapshots
    
    * Update src/components/Spotlight/Spotlight.stories.tsx
    
    Co-authored-by: Dawson Booth <dawsontaylorbooth@gmail.com>
    
    * Update src/components/Spotlight/Spotlight.tsx
    
    Co-authored-by: Dawson Booth <dawsontaylorbooth@gmail.com>
    
    * add issue links to TODO items
    
    ---------
    
    Co-authored-by: Dawson Booth <dawsontaylorbooth@gmail.com>

--- src/components/Spotlight/Spotlight.stories.tsx
+++ src/components/Spotlight/Spotlight.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Story, Meta } from '@storybook/react';
 import { mdiArrowRightBold, mdiCheckBold, mdiChevronDoubleRight, mdiDotsVertical } from '@mdi/js';
 import styled from 'styled-components';
@@ -13,7 +13,13 @@ const Header = styled(Card.NoPaddingHeader)`
 `;
 const Container = styled(Card.Container)`
   max-width: 35rem;
+`;
+const ScrollingWindowContainer = styled(Container)`
   margin-top: 10rem;
+`;
+const CustomScrollBody = styled(Card.Body)`
+  overflow-y: auto;
+  max-height: 70vh;
 `;
 
 const Annotation = styled(Spotlight.Annotation)`
@@ -27,11 +33,11 @@ const NextButtonContainer = styled(Button.Container)`
   justify-content: center;
 `;
 
-export const AnimatedSpotlight: Story = (args: Partial<SpotlightProps>) => {
+export const Default: Story = (args: Partial<SpotlightProps>) => {
   const [currStep, setStep] = useState<number>(0);
-  const [buttonRef, setButtonRef] = useState<HTMLElement>();
-  const [cardRef, setCardRef] = useState<HTMLElement>();
-  const [menuRef, setMenuRef] = useState<HTMLElement>();
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const cardRef = useRef<HTMLDivElement>(null);
+  const menuRef = useRef<HTMLButtonElement>(null);
   const [tourStarted, setTour] = useState<boolean>(false);
 
   const stepOptions = [null, menuRef, cardRef, buttonRef];
@@ -61,15 +67,11 @@ export const AnimatedSpotlight: Story = (args: Partial<SpotlightProps>) => {
     <>
       <Card
         StyledHeader={Header}
-        StyledContainer={Container}
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore - our ref types don't like getting a set state dispatch function
-        containerRef={setCardRef}
+        StyledContainer={ScrollingWindowContainer}
+        containerRef={cardRef}
         header={
           <Button
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore - our ref types don't like getting a set state dispatch function
-            containerRef={setMenuRef}
+            containerRef={menuRef}
             iconSuffix={mdiDotsVertical}
             variant={variants.text}
             color="black"
@@ -80,9 +82,7 @@ export const AnimatedSpotlight: Story = (args: Partial<SpotlightProps>) => {
             onClick={() => {
               setTour(true);
             }}
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore - our ref types don't like getting a set state dispatch function
-            containerRef={setButtonRef}
+            containerRef={buttonRef}
             color={colors.tertiary}
           >
             Start tour
@@ -109,7 +109,11 @@ export const AnimatedSpotlight: Story = (args: Partial<SpotlightProps>) => {
         </p>
       </Card>
       {tourStarted && (
-        <Spotlight {...args} StyledAnnotation={Annotation} targetElement={stepOptions[currStep]}>
+        <Spotlight
+          {...args}
+          StyledAnnotation={Annotation}
+          targetElement={stepOptions[currStep]?.current as HTMLElement}
+        >
           <Text color="white" containerProps={{ as: 'h1', style: { fontSize: '2em' } }}>
             {messages[currStep].title}
           </Text>
@@ -146,7 +150,133 @@ export const AnimatedSpotlight: Story = (args: Partial<SpotlightProps>) => {
     </>
   );
 };
-AnimatedSpotlight.args = {
+Default.args = {
+  padding: 12,
+  shape: 'rounded box',
+  animateTargetChanges: true,
+  backgroundDarkness: 0.3,
+  backgroundBlur: '0.25rem',
+  cornerRadius: 12,
+};
+
+export const CustomScrollWindow: Story = (args: Partial<SpotlightProps>) => {
+  const [currStep, setStep] = useState<number>(0);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const cardRef = useRef<HTMLDivElement>(null);
+  const bodyRef = useRef<HTMLDivElement>(null);
+  const menuRef = useRef<HTMLButtonElement>(null);
+  const [tourStarted, setTour] = useState<boolean>(false);
+
+  const stepOptions = [null, menuRef, cardRef, buttonRef];
+  const messages = [
+    {
+      title: 'Welcome to the tutorial!',
+      subtitle: 'targetElement is null for this part of the tour, so nothing is highlighted!',
+    },
+    { title: 'This is a kebab menu.', subtitle: '' },
+    { title: 'This is the whole card.', subtitle: '' },
+    { title: 'Press this button to restart the tour!', subtitle: '(you already knew that though)' },
+  ];
+
+  const goNext = () => {
+    setStep(step => {
+      if (step >= stepOptions.length - 1) {
+        setTour(false);
+        return 0;
+      }
+      return step + 1;
+    });
+  };
+
+  const lastStep = currStep === stepOptions.length - 1;
+
+  return (
+    <>
+      <Card
+        StyledHeader={Header}
+        StyledContainer={Container}
+        StyledBody={CustomScrollBody}
+        bodyRef={bodyRef}
+        containerRef={cardRef}
+      >
+        <Button
+          containerRef={menuRef}
+          iconSuffix={mdiDotsVertical}
+          variant={variants.text}
+          color="black"
+        />
+        <p>There are a few items in this card we can talk about!</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Ultricies mi eget mauris pharetra et ultrices neque
+          ornare aenean. Vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra.
+          Ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Mauris a diam maecenas
+          sed enim ut sem viverra aliquet. Ultrices in iaculis nunc sed augue lacus viverra. Sodales
+          neque sodales ut etiam. Pulvinar neque laoreet suspendisse interdum consectetur libero id
+          faucibus nisl. Ut placerat orci nulla pellentesque dignissim enim sit amet venenatis.
+          Vivamus arcu felis bibendum ut tristique. Netus et malesuada fames ac turpis egestas.
+          Porttitor rhoncus dolor purus non enim. Proin nibh nisl condimentum id. Aliquam ultrices
+          sagittis orci a scelerisque purus semper. Faucibus purus in massa tempor nec feugiat. Et
+          netus et malesuada fames. Cras pulvinar mattis nunc sed blandit libero. Nisi lacus sed
+          viverra tellus in. Tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. Id
+          porta nibh venenatis cras sed felis eget velit. Eros in cursus turpis massa tincidunt dui
+          ut ornare. Proin fermentum leo vel orci porta non. Quisque non tellus orci ac auctor
+          augue.
+        </p>
+        <Button
+          onClick={() => {
+            setTour(true);
+          }}
+          containerRef={buttonRef}
+          color={colors.tertiary}
+        >
+          Start tour
+        </Button>
+      </Card>
+      {tourStarted && (
+        <Spotlight
+          {...args}
+          // scrollingParentElement={bodyRef.current as HTMLElement}
+          StyledAnnotation={Annotation}
+          targetElement={stepOptions[currStep]?.current as HTMLElement}
+        >
+          <Text color="white" containerProps={{ as: 'h1', style: { fontSize: '2em' } }}>
+            {messages[currStep].title}
+          </Text>
+          <Text color="white" containerProps={{ style: { fontWeight: '700' } }}>
+            {messages[currStep].subtitle}
+          </Text>
+          <br />
+          <br />
+          <Button
+            color={colors.background}
+            iconSuffix={mdiChevronDoubleRight}
+            variant={variants.outline}
+            onClick={() => {
+              setStep(0);
+              setTour(false);
+            }}
+          >
+            Skip
+          </Button>
+          &nbsp;
+          <Button
+            StyledContainer={NextButtonContainer}
+            iconSuffix={lastStep ? mdiCheckBold : mdiArrowRightBold}
+            elevation={1}
+            color={lastStep ? colors.secondaryDark : colors.primaryDark}
+            onClick={() => {
+              goNext();
+            }}
+          >
+            {lastStep ? 'I got it' : 'Next'}
+          </Button>
+        </Spotlight>
+      )}
+    </>
+  );
+};
+CustomScrollWindow.args = {
   padding: 12,
   shape: 'rounded box',
   animateTargetChanges: true,
